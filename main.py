1105c5dfb16fe2d28651eb6b34c3e748bec297c9
Dmitriy <ka1deo@mail.ru>
2022-10-10 19:47:29 +0300

    FIRST COMMIT

---
+++ main.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+import psycopg2
+
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("DiAPP")
+        self.geometry('400x300')
+        self.resizable(False, False)
+
+    """ 
+
+    THIS PROJECT START AT 19:30 10.10.2022. 
+    I WANT TO SHOW YOU MY SKILLS IN OBJECT-ORIENTED PROGRAMMING
+    ALSO I WANT TO SHOW MY SKILLS IN PosgreSQL
+
+    """
+
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
